b72aa8a9d752c2f4de49071db11e5b88c18a400a
chann44 <44chansw@gmail.com>
2024-06-30 13:41:56 +0530

    feat: exception message changed

--- src/core/exceptions/http-exception.ts
+++ src/core/exceptions/http-exception.ts
@@ -1,10 +1,9 @@
-import { Messages, STATUS } from "../utils";
+import { STATUS } from "../utils";
 
 export class HttpException extends Error {
   public status: STATUS;
-  public message: Messages;
 
-  constructor(status: STATUS, message: Messages) {
+  constructor(status: STATUS, message: string) {
     super(message);
     this.status = status;
     this.message = message;
